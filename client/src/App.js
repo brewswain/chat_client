a7a4db0ff58834f1fa463b16e3757eeb3a1841d8
Brandon Lee <brandonlee2543@hotmail.com>
2020-04-08 08:22:40 -0400

    removed unused code from App.js as it was cluttering a very simple view, and the decisio nwas made to move the socket.io calls directly into our /chat path.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/home/home.component";
 import ChatPage from "./pages/chat/chat.component";
 
 function App() {
-  const userNameData = localStorage.getItem("persistedUserName");
-  const parsedUserData = JSON.parse(userNameData);
-
-  // const [socketState, setSocketState] = useState({
-  //   response: false,
-  //   endpoint: "https://127.0.0.1:3000"
-  // });
-
-  // useEffect(() => {
-  //   const socket = io();
-  // }, []);
-
   return (
     <div>
       <Switch>
-        <Route
-          exact
-          path="/"
-          // render={() =>
-          //   parsedUserData ? <Redirect to="/chat" /> : <HomePage />
-          // }
-          component={HomePage}
-        />
+        <Route exact path="/" component={HomePage} />
         <Route exact path="/chat" component={ChatPage} />
       </Switch>
     </div>
